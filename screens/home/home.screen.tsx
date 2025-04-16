b6dc7f8b52fd7644a897f140f3498726c0f4a609
GabrielPlus <gabrielpius0311@gmail.com>
2025-04-16 16:52:01 +0300

    spa categories section

--- screens/home/home.screen.tsx
+++ screens/home/home.screen.tsx
@@ -4,12 +4,18 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { useTheme } from '@/context/theme.context';
 import WelcomeHeader from '@/components/home/welcome.header';
 import HomeBanner from "@/components/home/home.banner";
+import { fontSizes, windowHeight, windowWidth } from '@/themes/app.constant';
+import { verticalScale } from 'react-native-size-matters';
+import GradiantText from '@/components/common/gradient.text';
+import SkeltonLoader from '@/utils/skeleton';
 
 
 
 
 export default function HomeScreen() {
     const { theme } = useTheme();
+    const [loading, setLoading] = useState(true);
+
 
     return (
         <>
@@ -19,7 +25,7 @@ export default function HomeScreen() {
                         ? ['#fff', '#fff']
                         : ['#fff', '#fff'] // duplicated for light mode
                 }
-                start={{ x: 0, y: 0}}
+                start={{ x: 0, y: 0}} 
                 end={{ x: 0, y: 1}}
                 style={{
                     flex:1,
@@ -29,6 +35,61 @@ export default function HomeScreen() {
                 <WelcomeHeader />
                 <ScrollView showsVerticalScrollIndicator={false}>
                     <HomeBanner />
+                    <View style={{
+                        marginHorizontal: windowWidth(20),
+                        marginTop: verticalScale(-25),
+                    }}>
+                        <View style={{
+                            flexDirection: "row", marginTop: windowHeight(5)
+                        }}>
+                                <Text style={{
+                                    fontSize: fontSizes.FONT35,
+                                    fontFamily: "Poppins_500Medium",
+                                    color: theme.dark ? "#000" : "#000"
+                                }}>
+                                    Spa {""}
+                                </Text>
+                                <GradiantText
+                                    text="Categories "
+                                    styles={{
+                                        fontSize: fontSizes.FONT35,
+                                        fontFamily: "Poppins_500Medium",
+                                    }}
+                                />
+                        </View>
+                        <View  style={{ flexDirection: "row", alignItems: "center" }}>
+                            <View
+                            style={{
+                                backgroundColor: "#12BB70",
+                                width: windowWidth(15),
+                                height: windowWidth(15),
+                                borderRadius: 100,
+                              }}
+                            />
+                             <Text
+                          style={{
+                            fontFamily: "Poppins_400Regular",
+                            fontSize: fontSizes.FONT18,
+                            paddingLeft: windowWidth(5),
+                            color: theme.dark ? "#000" : "#000",
+                          }}
+                        >
+                          explore our full range of hostilic spa services
+                        </Text>
+                        </View>
+                    </View>
+                    {
+                        loading ? (
+                            <>
+                            <SkeltonLoader />
+              <SkeltonLoader />
+                            </>
+                        ) : (
+                            <View>
+
+                            </View>
+                        )
+                    }
                 </ScrollView>
                 </LinearGradient>
 
